9313c6884923901d7de3c3b4e5242c0906f30449
murray-pocha <murray.pocha@gmail.com>
2025-01-12 19:31:37 -0700

    comment time-slot-routes

--- routes/time-slot-routes.js
+++ routes/time-slot-routes.js
@@ -19,7 +19,31 @@ router.post('/:event_id/timeslots', (req, res) => {
 
   console.log(`Time slot added to event ${event_id}:`, newTimeSlot);
 
-  res.status(201).send({ message: 'Time slot added.', timeSlots: mockTimeSlots[event_id]});
-})
+  res.status(201).send({ message: 'Time slot added.', timeSlots: mockTimeSlots[event_id] });
+});
+
+
+router.delete('/:event_id/timeslots/:timeslot_id', (req, res) => {
+  const { event_id, timeslot_id } = req.params;
+
+  if (!mockTimeSlots[event_id]) {
+    return res.status(404).send('Event or time slots not found.');
+  }
+  const timeSlotIndex = mockTimeSlots[event_id].findIndex(
+    (slot) => slot.id === timeslot_id
+  );
+
+  if (timeSlotIndex === -1) {
+    return res.status(404).send('Time slot not found.');
+  }
+  const removedSlot = mockTimeSlots[event_id].splice(timeSlotIndex, 1);
+
+  console.log(`Deleted time slot ${timeslot_id} from event ${event_id}:`, removedSlot);
+
+  res.status(200).send({
+    message: 'Time slot deleted successfully.',
+    removedSlot,
+  });
+});
 
 module.exports = router;
