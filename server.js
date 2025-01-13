062c3ab34fd995ee70deec251069252a76d650bb
murray-pocha <murray.pocha@gmail.com>
2025-01-13 12:49:35 -0700

    add console logs to debug server connections

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // load .env data into process.env
 require('dotenv').config();
 
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 // Web server config
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
